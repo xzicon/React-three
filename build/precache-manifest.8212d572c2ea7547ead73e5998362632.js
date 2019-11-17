432d8c0fef83c34abbecb02b1c7a23bc40cd7b30
xzicon <1637989675@qq.com>
2019-11-17 17:28:45 +0800

    build

---
+++ build/precache-manifest.8212d572c2ea7547ead73e5998362632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407e91cd8f52f985f29d252f6b1bf709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe75c80594252cc29b6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6d382f1fd120e01b979d",
+    "url": "/static/js/2.f6388305.chunk.js"
+  },
+  {
+    "revision": "0fe75c80594252cc29b6",
+    "url": "/static/js/main.bed6a805.chunk.js"
+  },
+  {
+    "revision": "9efa54b67bb26c5dafd7",
+    "url": "/static/js/runtime-main.2bc05d34.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
